d690a4ccf954da81242c6c79d210ec966715d1f6
diya-25-08 <diya.mondal4971@gmail.com>
2026-04-14 14:51:25 +0530

    Refactor calculator functions for clarity and structure

--- calculator.c
+++ calculator.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+//Functions for addition
 void Addition()
 {    
     int n,a[100],i,result=0;
+    //Asking number of inputs
     printf("How many numbers you want to add: ");
     scanf("%d",&n);
+    //Check valid numbers of inputs
     if(n<2)
     {
         printf("Addition is not possible with less than 2 numbers.\n");
@@ -12,32 +15,39 @@ void Addition()
         printf("\n-----------------------------\n");
         return;
     }
+    //taking input elements
     printf("Enter the numbers one by one: ");
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
+    //adding sum
     for(i=0;i<n;i++)
     {
        result+=a[i];
     }
+    //here we print the result
     printf("The result of ");
     for(i=0;i<n;i++)
     {
         printf(" %d ", a[i]);
         if(i<n-1)
-         printf("+");
+         printf("+"); //print + except last
     }
+    //final result
     printf(" is = %d\n",result); 
     printf("\n-----------------------------");
     printf("\n-----------------------------\n");
 
 }    
+//Funtion for subtraction
 void Subtraction()
 {
     int n,a[100],i,result;
+    //Asking numbers of inputs
     printf("How many numbers you want to subtract: ");
     scanf("%d",&n);
+    //Check valid numbers of inputs
     if(n<2)
     {
         printf("Subtraction is not possible with less than 2 numbers.\n");
@@ -45,32 +55,40 @@ void Subtraction()
         printf("\n-----------------------------\n");
         return;
     }
+    //Taking inputs elements
     printf("Enter the numbers one by one: ");
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
+    //initialize first number
     result=a[0];
+    //subtract remaining numbers
     for(i=1;i<n;i++)
     {
        result-=a[i];
     }
+    // printing expression
     printf("The result of ");
     for(i=0;i<n;i++)
     {
         printf(" %d ", a[i]);
         if(i<n-1)
-         printf("-");
+         printf("-"); // print - except last
     }
+    //final result
     printf(" is = %d\n",result); 
     printf("\n-----------------------------");
     printf("\n-----------------------------\n");
-}    
+} 
+//Function for multiplication 
 void Multiplication()
 {
    int n,a[100],i,result=1;
+    //Asking numbers of elements
     printf("How many numbers you want to multiply: ");
     scanf("%d",&n);
+    //Check valid numbers of input
     if(n<2)
     {
         printf("Multiplication is not possible with less than 2 numbers.\n");
@@ -78,22 +96,26 @@ void Multiplication()
         printf("\n-----------------------------\n");
         return;
     }
+    //taking the elements
     printf("Enter the numbers one by one: ");
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
+    // Multiply the numbers
     for(i=0;i<n;i++)
     {
        result*=a[i];
     }
+    // printing expression
     printf("The result of ");
     for(i=0;i<n;i++)
     {
         printf(" %d ", a[i]);
         if(i<n-1)
-         printf("*");
+         printf("*"); //print * except last
     }
+    //Final result
     printf(" is = %d\n",result); 
     printf("\n-----------------------------");
     printf("\n-----------------------------\n");
@@ -102,8 +124,10 @@ void Division()
 {
     int n,a[100],i;
     float result;
+    //input values
     printf("How many numbers you want to divide: ");
     scanf("%d",&n);
+    //validate input values
     if(n<2)
     {
         printf("Division is not possible with less than 2 numbers.\n");
@@ -111,15 +135,18 @@ void Division()
         printf("\n-----------------------------\n");
         return;
     }
+    
     printf("Enter the numbers one by one: ");
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
+    //assign first element
     result=a[0];
     for(i=1;i<n;i++)
     {  
-        if(a[i]==0)
+        //check division by 0
+        if(a[i]==0) 
         {
             printf("Division by zero is undefined.\n");
             printf("\n-----------------------------");
@@ -128,6 +155,7 @@ void Division()
         }
         result = result / a[i];
     }
+    //print expression
     printf("The result of ");
     for(i=0;i<n;i++)
     {
@@ -140,7 +168,7 @@ void Division()
     printf("\n-----------------------------");
 
 }
-//start from main function
+//starting of main function
 int main()
 {
     int ch;
